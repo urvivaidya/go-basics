26632a569dbf44770f8eaa2e792b16fe0a6534ab
Urvi Vaidya <urvivaidya@Urvis-MacBook-Air.local>
2020-04-02 00:35:45 +0530

    solved problem one

--- homework/problem-one/main.go
+++ homework/problem-one/main.go
@@ -7,10 +7,17 @@ func main() {
 	var input int
 	fmt.Scanf("%d", &input)
 
+	finalAnswer := 0
 	for i := 1; i <= input; i++ {
 		fmt.Println("Enter a number", i, ":")
 		var x int
 		fmt.Scanf("%d", &x)
+
+		if x > finalAnswer {
+			finalAnswer = x
+
+		}
 	}
+	fmt.Println("Answer", finalAnswer)
 
 }
